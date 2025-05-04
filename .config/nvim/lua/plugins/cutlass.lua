58fe1426d053e0e2ae5eec47448587159d784576
Jarrod Folino <jarrod.folino@fundingcircle.com>
2025-05-04 07:07:08 +0300

    Initial Commit

---
+++ .config/nvim/lua/plugins/cutlass.lua
@@ -0,0 +1,10 @@
+return {
+    "gbprod/cutlass.nvim",
+    config = function()
+      require("cutlass").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end
+  }
