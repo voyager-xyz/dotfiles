0949de11f45661fea380fa52fcb5c70c214ee590
Jarrod Folino <jarrod.folino@cultureamp.com>
2026-02-04 15:25:52 +1100

    added culture amp speciific stuff

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
-BASE_DIR="/Users/jarrodfolino/Code/dotfiles"
-stow -d $BASE_DIR -R -t ~ zshrc
-stow -d $BASE_DIR -R -t ~ starship
-stow -d $BASE_DIR -R -t ~ ghostty
-stow -d $BASE_DIR -R -t ~ atuin
-stow -d $BASE_DIR -R -t ~ zoxide
-stow -d $BASE_DIR -R -t ~ yazi
+BASE_DIR="/Users/jarrod.folino/Code/dotfiles"
+stow -d $BASE_DIR -R -t ~ zshrc --adopt
+stow -d $BASE_DIR -R -t ~ starship --adopt
+stow -d $BASE_DIR -R -t ~ ghostty --adopt
+stow -d $BASE_DIR -R -t ~ atuin --adopt
+stow -d $BASE_DIR -R -t ~ zoxide --adopt
+stow -d $BASE_DIR -R -t ~ yazi --adopt
 
 . ~/.zshrc
 . ~/.zshrc_func
